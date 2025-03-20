8bdbf9cd7a7f9cee60bae5ff35a09464805a7fe1
Aarushi Bhatia <aarushibhatia073@gmail.com>
2025-03-20 20:18:48 +0530

    connection

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,8 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import uploadHandler from "./upload.js";
-import askHandler from "./ask.js";
+import uploadHandler from "./api/upload";
+import askHandler from "./api/ask";
 
 dotenv.config();
 
