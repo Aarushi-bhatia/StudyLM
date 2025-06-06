3636ec33ebb228007beec962a164bd88586c6615
Aarushi-bhatia <aarushibhatia073@gmail.com>
2025-06-06 15:37:16 +0530

    change

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
-import FixedInput from "../components/FixedInput";
 import Nav from "../components/Nav";
 import Welcome from "../components/Welcome";
-import Chatpage from "./Chatpage";
 
 const Homepage = () => {
   const [answers, setAnswers] = useState([]);
@@ -12,15 +10,8 @@ const Homepage = () => {
     <div className="flex flex-col h-screen bg-[#2C2025] text-white">
       <Nav />
       <main className="flex-grow p-4">
-        {!isDocumentUploaded && answers.length === 0 ? (
           <Welcome />
-        ) : (
-          <Chatpage />
-        )}
       </main>
-
-      {/* Fixed input box at bottom */}
-      {isDocumentUploaded && <FixedInput />}
     </div>
   );
 };
