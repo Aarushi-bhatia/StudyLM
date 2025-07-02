7930701ac786c0ef1095598b5897af86e3dd869c
Aarushi-bhatia <aarushibhatia073@gmail.com>
2025-07-02 16:15:32 +0530

    added helmet

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,13 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import askRoutes from "./routes/ask.js";
 import authRoutes from "./routes/authRoutes.js"
+import helmet from "helmet";
 
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.use(helmet());
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
