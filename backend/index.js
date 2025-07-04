3c8d33964360e2e6a02871e18ac7bfd2844fd946
Himanshu <hv308489@gmail.com>
2025-07-05 02:56:47 +0530

    default route

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,10 @@ app.use(cors());
 app.use(express.json());
 app.use(helmet());
 
+app.get("/", (req, res) => {
+  res.send("running");
+});
+
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
