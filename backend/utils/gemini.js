6e410e8f2785b5d1aa21db5c8338bc9e3f207e86
Aarushi-bhatia <aarushibhatia073@gmail.com>
2025-07-06 14:32:57 +0530

    major changes

--- backend/utils/gemini.js
+++
@@ -1,7 +0,0 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
-import keyManager from "./geminiKeyManager.js";
-
-const key = keyManager.getRandomGeminiKey();
-const genAI = new GoogleGenerativeAI(key);
-
-export default genAI;
